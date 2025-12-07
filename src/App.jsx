e72488ee85f5b5b76b5fabfeac48e2ba0958d5b5
Umaima Zulfiqar <umaima749zulfiqar@gmail.com>
2025-12-07 15:56:55 +0500

    learning about props

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import Header from "./components/Header.jsx";
 import Entry from "./components/Entry.jsx";
 
 export default function App() {
+  const Firstname = "John";
   return (
     <>
-      <Header />
-      <Entry />
+      {/* <Header /> */}
+      {/* <Entry /> */}
+
+      <h1> hello {Firstname}</h1>   
+      {/* This is a sample line to show the use of Firstname variable */}
     </>
   )
 }
