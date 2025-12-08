f1a83c162bc218937c5367f13746ce7da8f3eb16
Umaima Zulfiqar <umaima749zulfiqar@gmail.com>
2025-12-08 16:57:13 +0500

    completed Contact practice of using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
 import Header from "./components/Header.jsx";
 import Entry from "./components/Entry.jsx";
+import Contact from "./components/Contact.jsx";
 
 export default function App() {
-  const Firstname = "John";
   return (
     <>
       {/* <Header /> */}
       {/* <Entry /> */}
 
-      <h1> It is currently about {new Date().getHours() % 12 }</h1> 
-      {/* The {} brackets contain the Javascript code */}
+      {/* Practice of passing data to Contacts */}
+      <div className="contacts"> 
+        <Contact 
+          img="./images/mr-whiskerson.png"
+          imgAlt="Photo of Mr. Whiskerson"
+          name="Mr. Whiskerson"
+          contact="(212) 555-1234"
+          email="mr.whiskaz@catnap.meow"
+        />
+        <Contact
+          img="./images/fluffykins.png"
+          imgAlt="Photo of Fluffykins"
+          name="Fluffykins"
+          contact="(212) 555-2345"
+          email="fluff@me.com"
+        />
+        <Contact 
+          img="./images/felix.png"
+          imgAlt="Photo of Felix"
+          name="Felix"
+          contact="(212) 555-4567"
+          email="thecat@hotmail.com"
+        />
+        <Contact 
+          img="./images/pumpkin.png"
+          imgAlt="Photo of Pumpkin"
+          name="Pumpkin"
+          contact="(0800) CAT KING"
+          email="pumpkin@scrimba.com"
+        />
+      </div>
     </>
   )
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,26 @@
+export default function Contact(props) { 
+  // this is how props are passed to components
+  return (
+    <article className="contact-card">
+        <img 
+            src={ props.img }
+            alt={ props.imgAlt }
+        />
+        <h3>{ props.name }</h3>
+        <div className="info-group">
+            <img 
+                src="./images/phone-icon.png" 
+                alt="phone icon" 
+            />
+            <p>{ props.contact }</p>
+        </div>
+        <div className="info-group">
+            <img 
+                src="./images/mail-icon.png" 
+                alt="mail icon"
+            />
+            <p>{ props.email }</p>
+        </div>
+    </article>
+  )
+}
