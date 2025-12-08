ad3766d6a650feaead5fca37295cff4e4fd83f53
Umaima Zulfiqar <umaima749zulfiqar@gmail.com>
2025-12-08 21:02:28 +0500

    Map the Entry component from the external file

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,31 @@ import Entry from "./components/Entry.jsx";
 import Japan from "./assets/japan.png";
 import Sydney from "./assets/sydney.png";
 import Geirangerfjord from "./assets/geirangerfjord.png";
+import data from "./Data.js";
 // import Contact from "./components/Contact.jsx";
 // import mrWhiskerson from "../images/mr-whiskerson.png"; // Loading image asset so that bundler can process it
 
 export default function App() {
+  const entryElement = data.map(entry => {
+    return (
+      <Entry 
+        key={entry.id}
+        img={entry.img} 
+        country={entry.country}
+        href={entry.googleMapsLink}
+        location={entry.title}
+        date={entry.dates}
+        description={entry.text}
+      />
+    )
+  })
+
   return (
     <>
       <Header />
-      <Entry 
-        img={{ src: Japan, alt: "Photo of Mount Fuji" }} 
-        country="Japan"
-        href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-        location="Mount Fuji"
-        date="12 Jan, 2021 - 24 Jan, 2021"
-        description="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-      />
-      <Entry 
-        img={{src: Sydney, alt: "Photo of Sydney Opera House"}}
-        country="AUSTRALIA"
-        href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-        location="Sydney Opera House"
-        date="27 May, 2023 - 8 Jun, 2023"
-        description="The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
-      />
-      <Entry 
-        img={{src: Geirangerfjord, alt: "Photo of Geirangerfjord"}}
-        country="NORWAY"
-        location="Geirangerfjord"
-        date="01 Oct, 2024 - 18 Nov, 2024"
-        description="The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
-      />
+      <main>
+        {entryElement}
+      </main>
 
       {/* Practice of passing data to Contacts */}
       {/* <div className="contacts"> 
