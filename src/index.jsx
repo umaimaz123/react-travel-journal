74fdd1b16e36e65b33779775fedad99e579d1236
Umaima Zulfiqar <umaima749zulfiqar@gmail.com>
2025-12-08 18:46:43 +0500

    Completed challenge of Map method to turn regular Javascript to JSX

--- src/index.jsx
+++ src/index.jsx
@@ -5,3 +5,30 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
     <App />
 )
+
+
+/* *** Challenge: USE ARRAY.MAP METHOD TO TURN REGULAR JAVASCRIPT TO JSX ELEMENTS: ***
+        
+            //  *************************************** //
+Given an array of strings, return an array of strings that wraps each
+of the original strings in an HTML-like <p></p> tag.
+
+E.g. given: ["Bulbasaur", "Charmander", "Squirtle"]
+return: ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+            //  *************************************** //
+*/
+
+// *** My solution: *** 
+//    ~~~~~~~~~~~~~
+// const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]
+
+// const pokemonElements = pokemon.map((pokemon) => {
+//   return `<p>${pokemon}</p>`
+// })
+
+// console.log(pokemonElements);
+
+// *** optimized solution: ***
+//    ~~~~~~~~~~~~~~~~~~~~
+// const pokemonElementsOptimized = pokemon.map(pokemon => `<p>${pokemon}</p>`)
+// console.log(pokemonElementsOptimized); 
